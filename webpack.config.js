df7d30d422352d4300eefb67dfb27ec2ee24b8ba
Pratik Raj <rajpratik71@gmail.com>
2020-01-31 19:23:34 +0530

    updated with more config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                
+            }
+        ]
+    }
+}
